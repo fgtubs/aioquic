0d095b971818eb267b09f6094bf889c6e4a4a77d
fgtubs <f.gentzen@tu-bs.de>
2021-11-19 18:37:14 +0100

    Added test data on http3 connection

--- callFunctionHTTP3.py
+++ callFunctionHTTP3.py
@@ -8,10 +8,12 @@ from numpy import random
 
 # Constants
 numOfCalls = 100
-numOfParallelProcesses = 100
+numOfParallelProcesses = 200
 
 def call():
     os.system("python3 /home/student/PycharmProjects/aioquic/examples/http3_client_modified.py --ca-certs tests/pycacert.pem wss://192.168.1.2:4433/ws")
+    # os.system("python3 /home/student/PycharmProjects/aioquic/examples/http3_client_modified.py --ca-certs tests/pycacert.pem wss://192.168.1.4:4433/ws")
+    # os.system("python3 /home/student/PycharmProjects/aioquic/examples/http3_client_modified.py --ca-certs tests/pycacert.pem wss://localhost:4433/ws")
 
 def calls_in_thread():
     time_delay = random.normal(1,0.15,numOfCalls)
